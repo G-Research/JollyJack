6d23b91c7a9d8776632618d06cf3496a1452f692
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-07-29 09:53:31 +0200

    read columns with a non-zero offset

--- python/test/test_jollyjack.py
+++ python/test/test_jollyjack.py
@@ -13,7 +13,7 @@ n_columns = 5
 n_rows = n_row_groups * chunk_size
 current_dir = os.path.dirname(os.path.realpath(__file__))
 
-def get_table():
+def get_table(n_rows, n_columns):
     # Generate a random 2D array of floats using NumPy
     # Each column in the array represents a column in the final table
     data = np.random.rand(n_rows, n_columns).astype(np.float32)
@@ -27,9 +27,10 @@ def get_table():
 class TestJollyJack(unittest.TestCase):
    
     def test_read_entire_table(self):
+
         with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as tmpdirname:
             path = os.path.join(tmpdirname, "my.parquet")
-            table = get_table()
+            table = get_table(n_rows, n_columns)
             pq.write_table(table, path, row_group_size=chunk_size, use_dictionary=False, write_statistics=True, store_schema=False, write_page_index=True)
 
             pr = pq.ParquetReader()
@@ -56,6 +57,38 @@ class TestJollyJack(unittest.TestCase):
             print("\nArray with data read directly into it:")
             print(np_array)
 
+            print("\nExpected data:")
+            print (expected_data.to_pandas().to_records(index=False))
+            self.assertTrue(np.array_equal(np_array, expected_data))
+
+    def test_read_nonzero_column_offset(self):
+        with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as tmpdirname:
+            path = os.path.join(tmpdirname, "my.parquet")
+            table = get_table(n_rows = chunk_size, n_columns = n_columns)
+            pq.write_table(table, path, row_group_size=chunk_size, use_dictionary=False, write_statistics=True, store_schema=False, write_page_index=True)
+
+            pr = pq.ParquetReader()
+            pr.open(path)
+            # Create an array of zeros
+            cols = 2
+            offset = n_columns - cols
+            np_array = np.zeros((chunk_size, cols), dtype='f', order='F')
+
+            print("\nEmpty array:")
+            print(np_array)
+            
+            jj.read_into_numpy_f32(metadata = pr.metadata
+                                    , parquet_path = path
+                                    , np_array = np_array
+                                    , row_group_idx = 0
+                                    , column_indices = range(offset, offset + cols))
+
+            print("\nArray with data read directly into it:")
+            print(np_array)
+
+
+            expected_data = pr.read_all(use_threads=False, column_indices = range(offset, offset + cols))
+            
             print("\nExpected data:")
             print (expected_data.to_pandas().to_records(index=False))
             self.assertTrue(np.array_equal(np_array, expected_data))
