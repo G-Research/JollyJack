93e4499d935bb019baf86643cbdd5534d12d4ffe
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-07-29 10:58:34 +0200

    hotfixing?

--- python/setup.py
+++ python/setup.py
@@ -94,6 +94,8 @@ setup(
     package_dir={"": "."},
     zip_safe=False,
     ext_modules=extensions,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
     test_suite = 'test',
     project_urls={
         "Documentation": "https://github.com/G-Research/PalletJack",
