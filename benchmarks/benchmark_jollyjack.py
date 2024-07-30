f7bc95981d24f76e66eefd632f8ebb53bc48a871
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-07-30 09:04:37 +0200

    pre_buffer

--- benchmarks/benchmark_jollyjack.py
+++ benchmarks/benchmark_jollyjack.py
@@ -8,7 +8,7 @@ import os
 
 row_groups = 1
 n_columns = 5_000
-chunk_size = 32_000
+chunk_size = 32
 n_rows = row_groups * chunk_size
 
 n_threads = 6
@@ -71,7 +71,8 @@ def worker_jollyjack_row_group():
     for r in range(row_groups):
         row_begin = r * chunk_size
         row_end = (r + 1) * chunk_size
-        jj.read_into_numpy_f32(metadata = pr.metadata, parquet_path = parquet_path, np_array = np_array[row_begin:row_end, :], row_group_idx = r, column_indices = all_columns)
+        jj.read_into_numpy_f32(metadata = pr.metadata, parquet_path = parquet_path, np_array = np_array[row_begin:row_end, :]
+                               , row_group_idx = r, column_indices = all_columns, pre_buffer=True)
 
     global jollyjack_numpy
     jollyjack_numpy = np_array
