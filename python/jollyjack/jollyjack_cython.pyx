7a7c6199703b904e31db5b96a9e2ab6126febdac
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-08-05 15:18:37 +0200

    tests working

--- python/jollyjack/jollyjack_cython.pyx
+++ python/jollyjack/jollyjack_cython.pyx
@@ -17,7 +17,7 @@ from libcpp cimport bool
 from libc.stdint cimport uint32_t
 from pyarrow._parquet cimport *
 
-cpdef void read_into_numpy_f32(parquet_path, FileMetaData metadata, cnp.ndarray np_array, row_group_idx, column_indices, pre_buffer=False):
+cpdef void read_into_numpy (parquet_path, FileMetaData metadata, cnp.ndarray np_array, row_group_idx, column_indices, pre_buffer=False):
     cdef string encoded_path = parquet_path.encode('utf8') if parquet_path is not None else "".encode('utf8')
     cdef vector[int] crow_group_indices = [row_group_idx]
     cdef vector[int] ccolumn_indices = column_indices
@@ -25,7 +25,7 @@ cpdef void read_into_numpy_f32(parquet_path, FileMetaData metadata, cnp.ndarray 
     cdef uint32_t cstride1_size = np_array.strides[1]
     cdef void* cdata = np_array.data
     cdef bool cpre_buffer = pre_buffer
-    cdef uint32_t cbuffer_size = np_array.shape[0] * np_array.shape[1] * np_array.strides[0]
+    cdef uint32_t cbuffer_size = np_array.shape[0] * np_array.strides[0] + np_array.shape[1] * np_array.strides[1]
 
     # Ensure the input is a 2D array
     assert np_array.ndim == 2, f"Unexpected np_array.ndim, {np_array.ndim} != 2"
--- python/test/test_jollyjack.py
+++ python/test/test_jollyjack.py
@@ -18,9 +18,9 @@ def get_table(n_rows, n_columns, data_type = pa.float32()):
     # Generate a random 2D array of floats using NumPy
     # Each column in the array represents a column in the final table
     data = np.random.rand(n_rows, n_columns).astype(np.float32)
-
+    
     # Convert the NumPy array to a list of PyArrow Arrays, one for each column
-    pa_arrays = [pa.array(data[:, i]) for i in range(n_columns)]
+    pa_arrays = [pa.array(data[:, i]).cast(data_type, safe = False) for i in range(n_columns)]
     schema = pa.schema([(f'column_{i}', data_type) for i in range(n_columns)])
     # Create a PyArrow Table from the Arrays
     return pa.Table.from_arrays(pa_arrays, schema=schema)
@@ -40,8 +40,6 @@ class TestJollyJack(unittest.TestCase):
             # Create an array of zeros
             np_array = np.zeros((n_rows, n_columns), dtype='f', order='F')
 
-            print("\nEmpty array:")
-            print(np_array)
             row_begin = 0
             row_end = 0
             
@@ -49,17 +47,12 @@ class TestJollyJack(unittest.TestCase):
                 row_begin = row_end
                 row_end = row_begin + pr.metadata.row_group(rg).num_rows
                 subset_view = np_array[row_begin:row_end, :] 
-                jj.read_into_numpy_f32(metadata = pr.metadata
+                jj.read_into_numpy (metadata = pr.metadata
                                        , parquet_path = path
                                        , np_array = subset_view
                                        , row_group_idx = rg
                                        , column_indices = range(pr.metadata.num_columns))
 
-            print("\nArray with data read directly into it:")
-            print(np_array)
-
-            print("\nExpected data:")
-            print (expected_data.to_pandas().to_records(index=False))
             self.assertTrue(np.array_equal(np_array, expected_data))
 
     def test_read_with_palletjack(self):
@@ -78,8 +71,6 @@ class TestJollyJack(unittest.TestCase):
             # Create an array of zeros
             np_array = np.zeros((n_rows, n_columns), dtype='f', order='F')
 
-            print("\nEmpty array:")
-            print(np_array)
             row_begin = 0
             row_end = 0
             
@@ -90,17 +81,12 @@ class TestJollyJack(unittest.TestCase):
                 row_begin = row_end
                 row_end = row_begin + metadata.num_rows
                 subset_view = np_array[row_begin:row_end, :] 
-                jj.read_into_numpy_f32(metadata = metadata
+                jj.read_into_numpy (metadata = metadata
                                        , parquet_path = path
                                        , np_array = subset_view
                                        , row_group_idx = 0
                                        , column_indices = column_indices)
 
-            print("\nArray with data read directly into it:")
-            print(np_array)
-
-            print("\nExpected data:")
-            print (expected_data.to_pandas().to_records(index=False))
             self.assertTrue(np.array_equal(np_array, expected_data))
 
     def test_read_nonzero_column_offset(self):
@@ -119,26 +105,19 @@ class TestJollyJack(unittest.TestCase):
             print("\nEmpty array:")
             print(np_array)
             
-            jj.read_into_numpy_f32(metadata = pr.metadata
+            jj.read_into_numpy (metadata = pr.metadata
                                     , parquet_path = path
                                     , np_array = np_array
                                     , row_group_idx = 0
                                     , column_indices = range(offset, offset + cols))
 
-            print("\nArray with data read directly into it:")
-            print(np_array)
-
-
             expected_data = pr.read_all(use_threads=False, column_indices = range(offset, offset + cols))
-            
-            print("\nExpected data:")
-            print (expected_data.to_pandas().to_records(index=False))
             self.assertTrue(np.array_equal(np_array, expected_data))
 
     def test_read_unsupported_column_types(self):
          with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as tmpdirname:
             path = os.path.join(tmpdirname, "my.parquet")
-            table = get_table(n_rows = chunk_size, n_columns = n_columns, data_type = pa.float64())
+            table = get_table(n_rows = chunk_size, n_columns = n_columns, data_type = pa.int32())
             pq.write_table(table, path, row_group_size=chunk_size, use_dictionary=False, write_statistics=True, store_schema=False, write_page_index=True)
 
             pr = pq.ParquetReader()
@@ -146,22 +125,15 @@ class TestJollyJack(unittest.TestCase):
             # Create an array of zerosx
             np_array = np.zeros((chunk_size, n_columns), dtype='f', order='F')
 
-            print("\nEmpty array:")
-            print(np_array)
-            
-            jj.read_into_numpy_f32(metadata = pr.metadata
+            with self.assertRaises(RuntimeError) as context:
+                jj.read_into_numpy (metadata = pr.metadata
                                     , parquet_path = path
                                     , np_array = np_array
                                     , row_group_idx = 0
                                     , column_indices = range(n_columns))
 
-            print("\nArray with data read directly into it:")
-            print(np_array)            
+                self.assertTrue(f"Column 0 has unsupported data type: 1!" in str(context.exception), context.exception)
 
-            expected_data = np.zeros((chunk_size, n_columns), dtype='f', order='F')
-            print("\nExpected data:")
-            print(expected_data)
-            self.assertTrue(np.array_equal(np_array, expected_data))
 
     def test_read_fp16(self):
          with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as tmpdirname:
@@ -174,21 +146,13 @@ class TestJollyJack(unittest.TestCase):
             # Create an array of zerosx
             np_array = np.zeros((chunk_size, n_columns), dtype=np.float16, order='F')
 
-            print("\nEmpty array:")
-            print(np_array)
-            
-            jj.read_into_numpy_f32(metadata = pr.metadata
+            jj.read_into_numpy (metadata = pr.metadata
                                     , parquet_path = path
                                     , np_array = np_array
                                     , row_group_idx = 0
                                     , column_indices = range(n_columns))
 
-            print("\nArray with data read directly into it:")
-            print(np_array)            
-
-            print("\nExpected data:")
             expected_data = pr.read_all().to_pandas().to_numpy()
-            print(expected_data)         
             self.assertTrue(np.array_equal(np_array, expected_data))
 
 if __name__ == '__main__':
