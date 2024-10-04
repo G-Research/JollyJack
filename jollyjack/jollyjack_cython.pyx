de68913f5536aff9cd466cfcd2ef773c50e230c4
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-10-04 14:17:21 +0200

    column names

--- jollyjack/jollyjack_cython.pyx
+++ jollyjack/jollyjack_cython.pyx
@@ -16,7 +16,7 @@ from pyarrow.lib cimport (get_reader)
 from collections.abc import Iterable
 
 def is_iterable_of_iterables(obj):
-    return isinstance(obj, Iterable) and all(isinstance(item, Iterable) for item in obj)
+    return isinstance(obj, Iterable) and all(isinstance(item, Iterable) and not isinstance(item, str) for item in obj)
 
 
 cpdef void read_into_torch (object source, FileMetaData metadata, tensor, row_group_indices, column_indices = [], column_names = [], pre_buffer = False, use_threads = True, use_memory_map = False):
@@ -45,29 +45,33 @@ cpdef void read_into_numpy (object source, FileMetaData metadata, cnp.ndarray np
     cdef void* cdata = np_array.data
     cdef bool cpre_buffer = pre_buffer
     cdef bool cuse_threads = use_threads
-    cdef vector[string] ccolumn_names = [c.encode('utf8') for c in column_names]
+    cdef vector[string] ccolumn_names
     cdef uint64_t cbuffer_size = (np_array.shape[0]) * cstride0_size + (np_array.shape[1] - 1) * cstride1_size
     cdef shared_ptr[CFileMetaData] c_metadata
+    cdef vector[int] ctarget_column_indices
+
     if metadata is not None:
         c_metadata = metadata.sp_metadata
 
-    target_column_indices = []
     if column_indices and isinstance(column_indices, dict):
         ccolumn_indices = column_indices.keys()
-        target_column_indices = column_indices.values()
+        ctarget_column_indices = column_indices.values()
     elif column_indices and is_iterable_of_iterables(column_indices):
         ccolumn_indices = [item[0] for item in column_indices]
-        target_column_indices = [item[1] for item in column_indices]
+        ctarget_column_indices = [item[1] for item in column_indices]
     elif column_indices:
-        ccolumn_indices = column_indices
         assert len(column_indices) == np_array.shape[1], f"Requested to read {len(column_indices)} columns, but the number of columns in numpy array is {np_array.shape[1]}"
-       
-    if isinstance(column_names, dict):
-        target_column_indices = column_names.values()
+        ccolumn_indices = column_indices
+
+    if column_names and isinstance(column_names, dict):
+        ccolumn_names = [c.encode('utf8') for c in column_names.keys()]
+        ctarget_column_indices = column_names.values()
+    elif column_names and is_iterable_of_iterables(column_names):
+        ccolumn_names = [item[0].encode('utf8') for item in column_names]
+        ctarget_column_indices = [item[1] for item in column_names]
     elif column_names:
         assert len(column_names) == np_array.shape[1], f"Requested to read {len(column_names)} columns, but the number of columns in numpy array is {np_array.shape[1]}"
-
-    cdef vector[int] ctarget_column_indices = target_column_indices
+        ccolumn_names = [c.encode('utf8') for c in column_names]
 
     # Ensure that only one input is set
     assert (column_indices or column_names) and (not column_indices or not column_names), f"Either column_indices or column_names needs to be set"
