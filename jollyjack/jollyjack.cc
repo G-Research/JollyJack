52a9b5661498785e238149e5f3ed572ca9fafdb0
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-11-26 22:24:30 +0100

    replace variant 1

--- jollyjack/jollyjack.cc
+++ jollyjack/jollyjack.cc
@@ -279,7 +279,7 @@ void TransposeShuffle (void* src_buffer, size_t src_stride0_size, size_t src_str
   uint8_t *dst_ptr = (uint8_t *)dst_buffer;
   const int BLOCK_SIZE = 32;
   char *env_value = getenv("JJ_TRANSPOSE_SHUFFLE");
-  int variant = 3;
+  int variant = 1;
   if (env_value != NULL)
   {
     variant = atoi(env_value);
@@ -297,14 +297,13 @@ void TransposeShuffle (void* src_buffer, size_t src_stride0_size, size_t src_str
   if (variant == 1)
   {
     for (int src_col = 0; src_col < src_cols; src_col++)
-    {
-      for (int src_row = 0; src_row < src_rows; src_row++)
-      {          
-        int dst_row = row_indices[src_col];
-        int dst_col = src_row;
-        size_t src_offset = src_stride0_size * src_row + src_stride1_size * src_col;
-        size_t dst_offset = dst_stride0_size * dst_row + dst_stride1_size * dst_col;
+    {    
+      int dst_row = row_indices[src_col];
+      size_t src_offset = src_stride1_size * src_col;
+      size_t dst_offset = dst_stride0_size * dst_row;
 
+      for (int src_row = 0; src_row < src_rows; src_row++, src_offset += src_stride0_size, dst_offset += dst_stride1_size)
+      {
         switch (src_stride0_size)
         {
           case 1:*(uint8_t*)&dst_ptr[dst_offset] = *(uint8_t*)&src_ptr[src_offset]; break;
@@ -317,6 +316,7 @@ void TransposeShuffle (void* src_buffer, size_t src_stride0_size, size_t src_str
   }
 
   // It turns out to be slower than the "variant 1"
+
   if (variant == 2)
   {  
     for (int block_col = 0; block_col < src_cols; block_col += BLOCK_SIZE)
@@ -347,25 +347,5 @@ void TransposeShuffle (void* src_buffer, size_t src_stride0_size, size_t src_str
     }
   }
   
-  if (variant == 3)
-  {
-    for (int src_col = 0; src_col < src_cols; src_col++)
-    {    
-      int dst_row = row_indices[src_col];
-      size_t src_offset = src_stride1_size * src_col;
-      size_t dst_offset = dst_stride0_size * dst_row;
-
-      for (int src_row = 0; src_row < src_rows; src_row++, src_offset += src_stride0_size, dst_offset += dst_stride1_size)
-      {
-        switch (src_stride0_size)
-        {
-          case 1:*(uint8_t*)&dst_ptr[dst_offset] = *(uint8_t*)&src_ptr[src_offset]; break;
-          case 2:*(uint16_t*)&dst_ptr[dst_offset] = *(uint16_t*)&src_ptr[src_offset]; break;
-          case 4:*(uint32_t*)&dst_ptr[dst_offset] = *(uint32_t*)&src_ptr[src_offset]; break;
-          case 8:*(uint64_t*)&dst_ptr[dst_offset] = *(uint64_t*)&src_ptr[src_offset]; break;
-        }
-      }
-    }
-  }
 
 }
