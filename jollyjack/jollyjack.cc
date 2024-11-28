a9194153872911f3fa2f8175b143f7d72000b190
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-11-28 14:46:16 +0100

    choosing the best variant

--- jollyjack/jollyjack.cc
+++ jollyjack/jollyjack.cc
@@ -279,7 +279,7 @@ void CopyToRowMajor (void* src_buffer, size_t src_stride0_size, size_t src_strid
   uint8_t *dst_ptr = (uint8_t *)dst_buffer;
   int BLOCK_SIZE = 32;
   char *env_value = getenv("JJ_copy_to_row_major");
-  int variant = 6;
+  int variant = 1;
   if (env_value != NULL)
   {
     variant = atoi(env_value);
@@ -295,137 +295,6 @@ void CopyToRowMajor (void* src_buffer, size_t src_stride0_size, size_t src_strid
   }
 
   if (variant == 1)
-  {
-    for (int src_col = 0; src_col < src_cols; src_col++)
-    {
-      size_t src_offset = src_stride1_size * src_col;
-
-      for (int src_row = 0; src_row < src_rows; src_row++, src_offset += src_stride0_size)
-      {
-        int dst_row = row_indices[src_row];
-        size_t dst_offset = dst_stride0_size * dst_row + dst_stride1_size * src_col;
-        switch (src_stride0_size)
-        {
-          case 1:*(uint8_t*)&dst_ptr[dst_offset] = *(uint8_t*)&src_ptr[src_offset]; break;
-          case 2:*(uint16_t*)&dst_ptr[dst_offset] = *(uint16_t*)&src_ptr[src_offset]; break;
-          case 4:*(uint32_t*)&dst_ptr[dst_offset] = *(uint32_t*)&src_ptr[src_offset]; break;
-          case 8:*(uint64_t*)&dst_ptr[dst_offset] = *(uint64_t*)&src_ptr[src_offset]; break;
-        }
-      }
-    }
-  }
-
-  if (variant == 2)
-  {
-    for (int src_row = 0; src_row < src_rows; src_row++)
-    {
-      int dst_row = row_indices[src_row];
-      size_t src_offset = src_stride0_size * src_row;
-      size_t dst_offset = dst_stride0_size * dst_row;
-      for (int src_col = 0; src_col < src_cols; src_col++, src_offset += src_stride1_size, dst_offset += dst_stride1_size)
-      {
-        switch (src_stride0_size)
-        {
-          case 1:*(uint8_t*)&dst_ptr[dst_offset] = *(uint8_t*)&src_ptr[src_offset]; break;
-          case 2:*(uint16_t*)&dst_ptr[dst_offset] = *(uint16_t*)&src_ptr[src_offset]; break;
-          case 4:*(uint32_t*)&dst_ptr[dst_offset] = *(uint32_t*)&src_ptr[src_offset]; break;
-          case 8:*(uint64_t*)&dst_ptr[dst_offset] = *(uint64_t*)&src_ptr[src_offset]; break;
-        }
-      }
-    }
-  }
-
-  // It turns out to be slower than the "variant 1"
-
-  if (variant == 3)
-  {  
-    for (int block_col = 0; block_col < src_cols; block_col += BLOCK_SIZE)
-    {            
-      int src_col_limit = std::min (src_cols, block_col + BLOCK_SIZE);
-      for (int block_row = 0; block_row < src_rows; block_row += BLOCK_SIZE)
-      {
-        int src_row_limit = std::min (src_rows, block_row + BLOCK_SIZE);
-        for (int src_col = block_col; src_col < src_col_limit; src_col++)
-        {
-          size_t src_offset = src_stride1_size * src_col + src_stride0_size * block_row;
-          for (int src_row = block_row; src_row < src_row_limit; src_row++, src_offset += src_stride0_size)
-          {
-            int dst_row = row_indices[src_row];
-            int dst_col = src_col;
-            size_t dst_offset = dst_stride0_size * dst_row + dst_stride1_size * dst_col;
-
-            switch (src_stride0_size)
-            {
-              case 1:*(uint8_t*)&dst_ptr[dst_offset] = *(uint8_t*)&src_ptr[src_offset]; break;
-              case 2:*(uint16_t*)&dst_ptr[dst_offset] = *(uint16_t*)&src_ptr[src_offset]; break;
-              case 4:*(uint32_t*)&dst_ptr[dst_offset] = *(uint32_t*)&src_ptr[src_offset]; break;
-              case 8:*(uint64_t*)&dst_ptr[dst_offset] = *(uint64_t*)&src_ptr[src_offset]; break;
-            }
-          }
-        }
-      }
-    }
-  }
-
-  if (variant == 4)
-  {  
-    for (int block_col = 0; block_col < src_cols; block_col += BLOCK_SIZE)
-    {            
-      int src_col_limit = std::min (src_cols, block_col + BLOCK_SIZE);
-      for (int block_row = 0; block_row < src_rows; block_row += BLOCK_SIZE)
-      {
-        int src_row_limit = std::min (src_rows, block_row + BLOCK_SIZE);
-        for (int src_row = block_row; src_row < src_row_limit; src_row++)
-        {
-          int dst_row = row_indices[src_row];
-          size_t src_offset = src_stride0_size * src_row + block_col * src_stride1_size;
-          size_t dst_offset = dst_stride0_size * dst_row + block_col * dst_stride1_size;
-          for (int src_col = block_col; src_col < src_col_limit; src_col++, dst_offset += dst_stride1_size, src_offset += src_stride1_size)
-          {
-            switch (src_stride0_size)
-            {
-              case 1:*(uint8_t*)&dst_ptr[dst_offset] = *(uint8_t*)&src_ptr[src_offset]; break;
-              case 2:*(uint16_t*)&dst_ptr[dst_offset] = *(uint16_t*)&src_ptr[src_offset]; break;
-              case 4:*(uint32_t*)&dst_ptr[dst_offset] = *(uint32_t*)&src_ptr[src_offset]; break;
-              case 8:*(uint64_t*)&dst_ptr[dst_offset] = *(uint64_t*)&src_ptr[src_offset]; break;
-            }
-          }
-        }
-      }
-    }
-  }
-
-  BLOCK_SIZE = 64;
-  if (variant == 5)
-  {
-    for (int block_col = 0; block_col < src_cols; block_col += BLOCK_SIZE)
-    {
-      int src_col_limit = std::min (src_cols, block_col + BLOCK_SIZE);
-      for (int block_row = 0; block_row < src_rows; block_row += BLOCK_SIZE)
-      {
-        int src_row_limit = std::min (src_rows, block_row + BLOCK_SIZE);
-        for (int src_row = block_row; src_row < src_row_limit; src_row++)
-        {
-          int dst_row = row_indices[src_row];
-          size_t src_offset = src_stride0_size * src_row + block_col * src_stride1_size;
-          size_t dst_offset = dst_stride0_size * dst_row + block_col * dst_stride1_size;
-          for (int src_col = block_col; src_col < src_col_limit; src_col++, dst_offset += dst_stride1_size, src_offset += src_stride1_size)
-          {
-            switch (src_stride0_size)
-            {
-              case 1:*(uint8_t*)&dst_ptr[dst_offset] = *(uint8_t*)&src_ptr[src_offset]; break;
-              case 2:*(uint16_t*)&dst_ptr[dst_offset] = *(uint16_t*)&src_ptr[src_offset]; break;
-              case 4:*(uint32_t*)&dst_ptr[dst_offset] = *(uint32_t*)&src_ptr[src_offset]; break;
-              case 8:*(uint64_t*)&dst_ptr[dst_offset] = *(uint64_t*)&src_ptr[src_offset]; break;
-            }
-          }
-        }
-      }
-    }
-  }
-
-  BLOCK_SIZE = 32;
-  if (variant == 6)
   {
     size_t src_offset_0 = 0;
     size_t dst_offset_0 = 0;
