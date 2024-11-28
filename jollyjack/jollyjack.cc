0143e486a7719505527b4af2e64763ea21ca1390
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-11-28 09:33:53 +0100

    2 variants

--- jollyjack/jollyjack.cc
+++ jollyjack/jollyjack.cc
@@ -279,7 +279,7 @@ void TransposeShuffle (void* src_buffer, size_t src_stride0_size, size_t src_str
   uint8_t *dst_ptr = (uint8_t *)dst_buffer;
   const int BLOCK_SIZE = 32;
   char *env_value = getenv("JJ_TRANSPOSE_SHUFFLE");
-  int variant = 1;
+  int variant = 2;
   if (env_value != NULL)
   {
     variant = atoi(env_value);
@@ -315,9 +315,29 @@ void TransposeShuffle (void* src_buffer, size_t src_stride0_size, size_t src_str
     }
   }
 
+  if (variant == 2)
+  {
+    for (int src_row = 0; src_row < src_rows; src_row++)
+    {
+      int dst_row = row_indices[src_row];
+      size_t src_offset = src_stride0_size * src_row;
+      size_t dst_offset = dst_stride0_size * dst_row;
+      for (int src_col = 0; src_col < src_cols; src_col++, src_offset += src_stride1_size, dst_offset += dst_stride1_size)
+      {
+        switch (src_stride0_size)
+        {
+          case 1:*(uint8_t*)&dst_ptr[dst_offset] = *(uint8_t*)&src_ptr[src_offset]; break;
+          case 2:*(uint16_t*)&dst_ptr[dst_offset] = *(uint16_t*)&src_ptr[src_offset]; break;
+          case 4:*(uint32_t*)&dst_ptr[dst_offset] = *(uint32_t*)&src_ptr[src_offset]; break;
+          case 8:*(uint64_t*)&dst_ptr[dst_offset] = *(uint64_t*)&src_ptr[src_offset]; break;
+        }
+      }
+    }
+  }
+
   // It turns out to be slower than the "variant 1"
 
-  if (variant == 2)
+  if (variant == 3)
   {  
     for (int block_col = 0; block_col < src_cols; block_col += BLOCK_SIZE)
     {            
