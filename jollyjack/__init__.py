b54cd0ac256390a122777dceeafcaaff0a18ec6d
Marcin Krystianc <marcin.krystianc@gmail.com>
2025-01-28 21:40:17 +0100

    pyarrow

--- jollyjack/__init__.py
+++ jollyjack/__init__.py
@@ -1,3 +1,23 @@
 # Importing pyarrow is necessary to load the runtime libraries
-import pyarrow.parquet
-from .jollyjack_cython import *
+
+import pyarrow
+
+# package/__init__.py
+try:
+    from importlib.metadata import version, requires
+    __version__ = version(__package__)  # Uses package metadata
+    dependencies = requires(__package__)
+    print(dependencies)  # List of requirements strings
+
+except ImportError:
+    __version__ = "0.0.1"  # Fallback
+
+
+try:
+    from .jollyjack_cython import *
+        
+except ImportError as e:
+    if "libarrow.so" in str(e):
+        raise ImportError(f"Unable to load libarrow.so. This version of {__package__}={__version__} is built against arrow  ensure you have pyarrow==17.x installed. Current pyarrow version: {pyarrow.__version__}")
+    else:
+        raise
