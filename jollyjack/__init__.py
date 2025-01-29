34f86401e2ac0dfebac0dc4f6e4ac7b21a58c7e3
Marcin Krystianc <marcin.krystianc@gmail.com>
2025-01-29 09:12:39 +0100

    final version?

--- jollyjack/__init__.py
+++ jollyjack/__init__.py
@@ -5,5 +5,11 @@ from importlib.metadata import version, requires
 __version__ = version(__package__)  # Uses package metadata
 dependencies = requires(__package__)
 pyarrow_req = next((r for r in dependencies if r.startswith('pyarrow')), '')
+try:
 from .jollyjack_cython import *
+except ImportError as e:
+    if any(x in str(e) for x in ['arrow', 'parquet']):
+        raise ImportError(f"This version of {__package__}={__version__} is built against {pyarrow_req}, please ensure you have it installed. Current pyarrow version is {pyarrow.__version__}")
+    else:
+        raise
 
