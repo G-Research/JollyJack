4e3644fc270a4b49d9b74181e8613cd5234029e3
Marcin Krystianc <marcin.krystianc@gmail.com>
2025-01-29 08:43:48 +0100

    more imports

--- jollyjack/__init__.py
+++ jollyjack/__init__.py
@@ -1,4 +1,6 @@
 # Importing pyarrow is necessary to load the runtime libraries
 import pyarrow
 import pyarrow.parquet
+from importlib.metadata import version, requires
 from .jollyjack_cython import *
+
