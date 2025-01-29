15c41ae1e91f7819866871c7d275663898f5bf0a
Marcin Krystianc <marcin.krystianc@gmail.com>
2025-01-29 08:57:00 +0100

    more code

--- jollyjack/__init__.py
+++ jollyjack/__init__.py
@@ -2,5 +2,8 @@
 import pyarrow
 import pyarrow.parquet
 from importlib.metadata import version, requires
+__version__ = version(__package__)  # Uses package metadata
+dependencies = requires(__package__)
+pyarrow_req = next((r for r in dependencies if r.startswith('pyarrow')), '')
 from .jollyjack_cython import *
 
