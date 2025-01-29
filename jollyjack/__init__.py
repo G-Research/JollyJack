3a48021b8fa035b6e98015588cfd5e24d674af02
Marcin Krystianc <marcin.krystianc@gmail.com>
2025-01-29 08:27:34 +0100

    windows only

--- jollyjack/__init__.py
+++ jollyjack/__init__.py
@@ -1,16 +1,3 @@
 # Importing pyarrow is necessary to load the runtime libraries
-
-import pyarrow
-
-from importlib.metadata import version, requires
-__version__ = version(__package__)  # Uses package metadata
-dependencies = requires(__package__)
-pyarrow_req = next((r for r in dependencies if r.startswith('pyarrow')), '')
-
-try:
-    from .jollyjack_cython import *
-except ImportError as e:
-    if any(x in str(e) for x in ['arrow', 'parquet']):
-        raise ImportError(f"This version of {__package__}={__version__} is built against {pyarrow_req}, please ensure you have it installed. Current pyarrow version is {pyarrow.__version__}")
-    else:
-        raise
+import pyarrow.parquet
+from .jollyjack_cython import *
