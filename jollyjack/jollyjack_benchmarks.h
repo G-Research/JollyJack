f7fa94beea02d093150fb9ca5e856cc9828fe1b7
Marcin Krystianc <marcin.krystianc@gmail.com>
2025-10-13 18:47:54 +0200

    ReadIntoMemory_benchmark5

--- jollyjack/jollyjack_benchmarks.h
+++ jollyjack/jollyjack_benchmarks.h
@@ -64,5 +64,20 @@ void ReadIntoMemory_benchmark4 (const std::string& path
     , int64_t expected_rows
     , arrow::io::CacheOptions cache_options);
 
+void ReadIntoMemory_benchmark5 (const std::string& path
+    , std::shared_ptr<parquet::FileMetaData> file_metadata
+    , void* buffer
+    , size_t buffer_size
+    , size_t stride0_size
+    , size_t stride1_size
+    , std::vector<int> column_indices
+    , const std::vector<int> &row_groups
+    , const std::vector<int64_t> &target_row_ranges
+    , const std::vector<std::string> &column_names
+    , const std::vector<int> &target_column_indices
+    , bool pre_buffer
+    , bool use_threads
+    , int64_t expected_rows
+    , arrow::io::CacheOptions cache_options);
 
 
