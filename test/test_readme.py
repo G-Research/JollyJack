7a36ca760779c27a84ca986fcbf09cc3fa814804
Marcin Krystianc <marcin.krystianc@gmail.com>
2026-02-03 13:07:56 +0100

    JJ_READER_BACKEND

--- test/test_readme.py
+++ test/test_readme.py
@@ -14,7 +14,7 @@ import os
 import unittest
 from pyarrow import fs
 
-if os.environ.get('JJ_EXPERIMENTAL_READER') != None:
+if os.environ.get('JJ_READER_BACKEND') != None:
   pytest.skip("io_uring is enabled but this test is not compatible with io_uring", allow_module_level=True)
 
 
